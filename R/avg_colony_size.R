e3adbd8ed834a4cbf442c838f8d1d12d19d3b9c9
icrichmond <bellarichmond@proton.me>
2026-05-01 15:35:18 -0400

    replacement for sum_count

---
+++ R/avg_colony_size.R
@@ -0,0 +1,22 @@
+avg_colony_size <- function(full_datasets, filter_years){
+
+  count_avgs <- full_datasets[['counts']] %>%
+    group_by(island, year) %>%
+    filter(year %in% filter_years) %>%
+    summarize(adults_avg = mean(adults, na.rm = T),
+              chicks_avg = mean(chicks, na.rm = T))
+
+  size_avgs <- full_datasets[['penguins']] %>%
+    group_by(island, sex, year) %>%
+    filter(year %in% filter_years) %>%
+    summarize(body_mass_g_avg = mean(body_mass_g, na.rm = T),
+              flipper_length_mm_avg = mean(flipper_length_mm, na.rm = T))
+
+
+  avgs <- list(count_avgs, size_avgs)
+
+  names(avgs) <- names(full_datasets)
+
+  return(avgs)
+
+}

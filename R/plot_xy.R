cc7b935b53d526d043828c048fd81158039c061d
icrichmond <bellarichmond@proton.me>
2026-05-04 12:08:53 -0400

    use geom smooth

--- R/plot_xy.R
+++ R/plot_xy.R
@@ -5,7 +5,7 @@ plot_xy <- function(DT, x_col, y_col, color_col) {
   #stopifnot(color_col %in% colnames(DT))
 
   g <- if (missing(color_col)) geom_point() else geom_point(aes(color = .data[[color_col]]))
-  h <- if (missing(color_col)) geom_line() else geom_line(aes(color = .data[[color_col]]))
+  h <- if (missing(color_col)) geom_smooth(method = 'lm') else geom_smooth(method = 'lm', aes(color = .data[[color_col]]))
 
   ggplot(DT, aes(.data[[x_col]], .data[[y_col]])) + g + h
 

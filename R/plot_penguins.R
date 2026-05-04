1dba0b2431f44d9c948f13349001b3777ea4096f
Alec L. Robitaille <robit.alec@gmail.com>
2026-05-04 17:47:54 -0230

    fix missing ggtitle

--- R/plot_penguins.R
+++ R/plot_penguins.R
@@ -1,5 +1,6 @@
-plot_penguins <- function(penguins) {
+plot_penguins <- function(penguins, keys) {
   ggplot(penguins, aes(mean_precipitation_mm, body_mass_g, color = sex)) +
     geom_point() +
-    geom_smooth(method = 'lm')
+    geom_smooth(method = 'lm') +
+    ggtitle(keys)
 }

117c064ddf32fc8aeca2720c1ead67ed6f77976f
Alec L. Robitaille <robit.alec@gmail.com>
2026-05-04 17:44:55 -0230

    split prepare_csv into merge_counts_weather and penguin

---
+++ R/merge_counts_weather.R
@@ -0,0 +1,19 @@
+merge_counts_weather <- function(counts, weather) {
+  # Check input
+  stopifnot(is.data.frame(counts))
+  stopifnot(is.data.frame(weather))
+
+  # Clean column names, extract year + month, join with weather
+  prep_counts <- counts %>%
+    clean_names() %>%
+    mutate(year = year(as.Date(date)),
+           month = month(as.Date(date)))
+
+  # Clean column names and extract year + month
+  prep_weather <- weather %>%
+    clean_names() %>%
+    mutate(year = year(as.Date(date)),
+           month = month(as.Date(date)))
+
+  inner_join(prep_counts, prep_weather, by = c('year', 'month'))
+}

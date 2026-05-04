bc7bfe6b6ddfb51c1f9ca6679136b20d23c5d283
icrichmond <bellarichmond@proton.me>
2026-05-04 11:23:46 -0400

    incorporate weather

--- R/avg_colony_size.R
+++ R/avg_colony_size.R
@@ -4,13 +4,15 @@ avg_colony_size <- function(full_datasets, filter_years){
     group_by(island, year) %>%
     filter(year %in% filter_years) %>%
     summarize(adults_avg = mean(adults, na.rm = T),
-              chicks_avg = mean(chicks, na.rm = T))
+              chicks_avg = mean(chicks, na.rm = T),
+              mean_temperature_c_yearly = mean(mean_temperature_c, na.rm = T))
 
   size_avgs <- full_datasets[['penguins']] %>%
     group_by(island, sex, year) %>%
     filter(year %in% filter_years) %>%
     summarize(body_mass_g_avg = mean(body_mass_g, na.rm = T),
-              flipper_length_mm_avg = mean(flipper_length_mm, na.rm = T))
+              flipper_length_mm_avg = mean(flipper_length_mm, na.rm = T),
+              mean_precipitation_mm_yearly = mean(mean_precipitation_mm, na.rm = T))
 
 
   avgs <- list(count_avgs, size_avgs)

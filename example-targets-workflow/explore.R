ce0d5858c72fa91e3fd6b346b1d123530eb8d8cb
Alec L. Robitaille <robit.alec@gmail.com>
2026-05-07 17:42:28 -0230

    add commands for demo

--- example-targets-workflow/explore.R
+++ example-targets-workflow/explore.R
@@ -1,10 +1,22 @@
 library(targets)
 tar_source('R')
 
-
-# Visnetwork
-tar_visnetwork()
-
+# Look at the workflow
 tar_visnetwork(targets_only = TRUE)
 
-tar_visnetwork(starts_with('plot'))
+# Run the workflow
+tar_make()
+
+# Read an output
+tar_read(prepared_counts)
+
+tar_read(fig_counts, branches = 1)
+
+# Load an output
+tar_load(prepared_penguins)
+
+ggplot(prepared_penguins) +
+  geom_histogram(aes(flipper_length_mm))
+
+# Look at the metadata
+tar_meta()

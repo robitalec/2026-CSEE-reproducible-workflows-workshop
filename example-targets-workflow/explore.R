0ff97e3d415e0ba83b2363e3c2c562378975fe5c
Bella Richmond <bellarichmond@proton.me>
2026-05-07 15:27:59 -0400

    update explore to match example workflow

--- example-targets-workflow/explore.R
+++ example-targets-workflow/explore.R
@@ -7,4 +7,24 @@ tar_visnetwork()
 
 tar_visnetwork(targets_only = TRUE)
 
-tar_visnetwork(starts_with('plot'))
+tar_visnetwork(ends_with('counts'))
+
+
+# Make
+tar_make()
+
+
+# Load
+tar_read(prepared_counts)
+
+tar_load(mean_counts_weather)
+
+tar_read(fig_counts)
+
+
+# Metadata
+tar_meta()
+
+tar_meta(ends_with('counts'))
+
+View(tar_meta())

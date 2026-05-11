4aec5192d3631d6b110956fed3116d14252ed2e7
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-05-11 17:14:11 +0000

    Built site for gh-pages

--- projects/example-targets-workflow/R/plot_penguins.R
+++
@@ -1,6 +0,0 @@
-plot_penguins <- function(penguins, keys) {
-  ggplot(penguins, aes(mean_precipitation_mm, body_mass_g, color = sex)) +
-    geom_point() +
-    geom_smooth(method = 'lm') +
-    ggtitle(keys)
-}
